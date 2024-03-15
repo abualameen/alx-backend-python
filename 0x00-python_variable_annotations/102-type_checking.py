c9b9c8f462d9f97b702eea0341c58c4b6334669c
abu al ameen <abulyaqs@gmail.com>
2024-03-15 01:30:23 +0100

    task 101 correction

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -8,16 +8,16 @@ repeating each element.
 from typing import Tuple, List, Any
 
 
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zooms in on an array by repeating each element.
 
     Args:
-        lst (Tuple[Any, ...]): The input array.
+        lst (Tuple): The input array.
         factor (int, optional): The zoom factor. Defaults to 2.
 
     Returns:
-        Tuple[Any, ...]: The zoomed-in array.
+        List: The zoomed-in array.
     """
-    zoomed_in = tuple(item for item in lst for _ in range(factor))
+    zoomed_in = [item for item in lst for _ in range(factor)]
     return zoomed_in
